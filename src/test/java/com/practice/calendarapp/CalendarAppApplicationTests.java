4b0bfe58eb814a3a2c3a543f9406da90ce235744
Katie <cope_katie@yahoo.com>
2023-08-24 16:15:17 -0500

    Initial commit

---
+++ src/test/java/com/practice/calendarapp/CalendarAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.calendarapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
